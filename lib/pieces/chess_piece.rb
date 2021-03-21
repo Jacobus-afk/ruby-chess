53f3254ac5955a0b631b497540157fdf92491416
Jacobus-afk <s24162826@gmail.com>
2021-03-21 21:11:52 +0200

    added PIECE_DATA with info about each piece

--- lib/pieces/chess_piece.rb
+++ lib/pieces/chess_piece.rb
@@ -3,15 +3,27 @@
 WHITE_PIECE = 0
 BLACK_PIECE = 1
 
+PIECE_DATA = { '♖' => { WHITE_PIECE => { unicode: '♖', start_pos: %w[a1 h1] },
+                        BLACK_PIECE => { unicode: '♜', start_pos: %w[a8 h8] } },
+               '♘' => { WHITE_PIECE => { unicode: '♘', start_pos: %w[b1 g1] },
+                        BLACK_PIECE => { unicode: '♞', start_pos: %w[b8 g8] } },
+               '♗' => { WHITE_PIECE => { unicode: '♗', start_pos: %w[c1 f1] },
+                        BLACK_PIECE => { unicode: '♝', start_pos: %w[c8 f8] } },
+               '♕' => { WHITE_PIECE => { unicode: '♕', start_pos: %w[d1] },
+                        BLACK_PIECE => { unicode: '♛', start_pos: %w[d8] } },
+               '♔' => { WHITE_PIECE => { unicode: '♔', start_pos: %w[e1] },
+                        BLACK_PIECE => { unicode: '♚', start_pos: %w[e8] } },
+               '♙' => { WHITE_PIECE => { unicode: '♙', start_pos: %w[a2 b2 c2 d2 e2 f2 g2 h2] },
+                        BLACK_PIECE => { unicode: '♟', start_pos: %w[a7 b7 c7 d7 e7 f7 g7 h7] } } }.freeze
+
 # chess piece class
 class ChessPiece
-  include Teams
-
   attr_reader :team, :unicode, :position
 
   def initialize(team, icon, pos)
     @team = team
-    @unicode = (icon.ord + (6 * team)).chr(Encoding::UTF_8)
+    # @unicode = (icon.ord + (6 * team)).chr(Encoding::UTF_8)
+    @unicode = PIECE_DATA[icon][team][:unicode]
     @position = pos
     @active = true
   end
