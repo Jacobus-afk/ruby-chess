edf9824a0df873da58d7a9f4d1320120009b6f29
Jacobus-afk <s24162826@gmail.com>
2021-04-08 00:26:20 +0200

    refactored #draw_board - now uses same variable for both tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,6 +16,8 @@ describe Board do
   white = "\e[30;107m   \e[0m"
   black = "\e[30;100m   \e[0m"
   describe '#draw_board' do
+    # let(:inactive) { instance_double('ChessPiece', coordinate: [0, 0], unicode: '♕') }
+    let(:chesspiece) { instance_double('ChessPiece', coordinate: [1, 2], unicode: '♘') }
     it 'draws template correctly' do
       expect(board.draw_board).to eql(["#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
                                        "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
@@ -28,9 +30,8 @@ describe Board do
     end
 
     it 'handles inactive pieces correctly' do
-      inactive = instance_double('ChessPiece', coordinate: [0, 0], unicode: '♕')
-      board.instance_variable_set(:@pieces, { 'a8' => inactive })
-      allow(inactive).to receive(:active?).and_return(false)
+      board.instance_variable_set(:@pieces, { 'a8' => chesspiece })
+      allow(chesspiece).to receive(:active?).and_return(false)
 
       expect(board.draw_board).to eql(["#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
                                        "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
@@ -43,9 +44,8 @@ describe Board do
     end
 
     it 'handles active pieces correctly' do
-      active = instance_double('ChessPiece', coordinate: [1, 2], unicode: '♘')
-      board.instance_variable_set(:@pieces, { 'c7' => active })
-      allow(active).to receive(:active?).and_return(true)
+      board.instance_variable_set(:@pieces, { 'c7' => chesspiece })
+      allow(chesspiece).to receive(:active?).and_return(true)
 
       expect(board.draw_board).to eql(["#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
                                        "#{black}#{white}" + ' ♘ '.black_bg + "#{white}#{black}#{white}#{black}#{white}",
