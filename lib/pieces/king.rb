daf44492857451de57ac5b597baf75ab03254695
Jacobus-afk <s24162826@gmail.com>
2021-04-10 17:06:48 +0200

    added generate_possible_moves functionality and test for king piece

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -12,4 +12,62 @@ class King < ChessPiece
   def castling?
     @castling
   end
+
+  def generate_possible_moves
+    super
+    _fill_paths_arr
+  end
+
+  private
+
+  def _add_to_possible_paths(*paths)
+    paths.each do |path|
+      next if path.nil?
+
+      @possible_paths.push(path)
+    end
+  end
+
+  def _create_path(move, tags)
+    pos = find_position(move)
+    return if pos.nil?
+
+    Node.new(pos => tags)
+  end
+
+  def _add_castling_paths(tags = %i[check_move check_castling])
+    return unless first_move?
+
+    x = @coordinate[1]
+    y = @coordinate[0]
+
+    _add_to_possible_paths(_create_path([y, x + 2], tags),
+                           _create_path([y, x - 2], tags))
+  end
+
+  def _add_normal_paths(tags = %i[check_move check_attack])
+    x = @coordinate[1]
+    y = @coordinate[0]
+
+    (-1..1).each do |i|
+      (-1..1).each do |j|
+        next if i.zero? && j.zero?
+
+        _add_to_possible_paths(_create_path([y + j, x + i], tags))
+      end
+    end
+  end
+
+  def _fill_paths_arr
+    _add_castling_paths
+    _add_normal_paths
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  piece = King.new(WHITE_PIECE, 'e1')
+  piece.generate_possible_moves
+  piece.move('e2')
+  piece.generate_possible_moves
+  puts
 end
--- spec/pieces/king_spec.rb
+++ spec/pieces/king_spec.rb
@@ -1,14 +1,44 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
+
 require './lib/pieces/king'
+require './spec/pieces/linked_list_helper'
 
 describe King do
+  subject(:white_king) { described_class.new(WHITE_PIECE, 'e1') }
+  subject(:black_king) { described_class.new(BLACK_PIECE, 'e8') }
+
   context 'when instantiating class' do
-    subject(:white_king) { described_class.new(WHITE_PIECE, 'e1') }
-    subject(:black_king) { described_class.new(BLACK_PIECE, 'e8') }
     it 'castled is set to false' do
       expect(white_king).not_to be_castling
       expect(black_king).not_to be_castling
     end
   end
+  context 'for class functions' do
+    before(:each) do
+      white_king.generate_possible_moves
+      black_king.generate_possible_moves
+    end
+    describe '#generate_possible_moves' do
+      it "king's first move" do
+        white_positions = extract_path_positions(white_king.possible_paths)
+        black_positions = extract_path_positions(black_king.possible_paths)
+        expect(white_positions).to contain_exactly('d1', 'c1', 'f1', 'g1', 'd2', 'e2', 'f2')
+        expect(black_positions).to contain_exactly('d8', 'c8', 'f8', 'g8', 'd7', 'e7', 'f7')
+      end
+      it "not king's first move" do
+        white_king.move('e2')
+        black_king.move('e7')
+        white_king.generate_possible_moves
+        black_king.generate_possible_moves
+        white_positions = extract_path_positions(white_king.possible_paths)
+        black_positions = extract_path_positions(black_king.possible_paths)
+        expect(white_positions).to contain_exactly('d1', 'e1', 'f1', 'd2', 'f2', 'd3', 'e3', 'f3')
+        expect(black_positions).to contain_exactly('d8', 'e8', 'f8', 'd7', 'f7', 'd6', 'e6', 'f6')
+      end
+    end
+  end
 end
+
+# rubocop:enable Metrics/BlockLength
