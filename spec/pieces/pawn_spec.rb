bb193fd9568fdb486aa0f4af037550a78845ef81
Jacobus-afk <s24162826@gmail.com>
2021-04-11 00:30:06 +0200

    refactored pawn spec to make it more concise

--- spec/pieces/pawn_spec.rb
+++ spec/pieces/pawn_spec.rb
@@ -34,89 +34,71 @@ describe Pawn do
       pawn_right.generate_possible_moves
     end
     describe '#generate_possible_moves' do
-      context 'pawn doing its first move' do
-        it 'in centre of board' do
-          possible_positions = extract_path_positions(pawn_centre.possible_paths)
-          expect(possible_positions).to contain_exactly('c6', 'b6', 'c5', 'd6')
-        end
-        it 'on left side of board' do
-          possible_positions = extract_path_positions(pawn_left.possible_paths)
-          expect(possible_positions).to contain_exactly('b6', 'a6', 'a5')
-        end
-        it 'on right side of board' do
-          possible_positions = extract_path_positions(pawn_right.possible_paths)
-          expect(possible_positions).to contain_exactly('h3', 'h4', 'g3')
-        end
+      it 'pawn doing its first move' do
+        centre_positions = extract_path_positions(pawn_centre.possible_paths)
+        left_positions = extract_path_positions(pawn_left.possible_paths)
+        right_positions = extract_path_positions(pawn_right.possible_paths)
+
+        expect(centre_positions).to contain_exactly('c6', 'b6', 'c5', 'd6')
+        expect(left_positions).to contain_exactly('b6', 'a6', 'a5')
+        expect(right_positions).to contain_exactly('h3', 'h4', 'g3')
       end
-      context "not pawn's first move" do
-        it 'in center of board' do
-          pawn_centre.move('c6')
-          pawn_centre.generate_possible_moves
-          possible_positions = extract_path_positions(pawn_centre.possible_paths)
-          expect(possible_positions).to contain_exactly('b5', 'd5', 'c5')
-        end
-        it 'on left side of board' do
-          pawn_left.move('a6')
-          pawn_left.generate_possible_moves
-          possible_positions = extract_path_positions(pawn_left.possible_paths)
-          expect(possible_positions).to contain_exactly('a5', 'b5')
-        end
-        it 'on right side of board' do
-          pawn_right.move('h3')
-          pawn_right.generate_possible_moves
-          possible_positions = extract_path_positions(pawn_right.possible_paths)
-          expect(possible_positions).to contain_exactly('g4', 'h4')
-        end
+      it "not pawn's first move" do
+        pawn_centre.move('c6')
+        pawn_left.move('a6')
+        pawn_right.move('h3')
+        pawn_centre.generate_possible_moves
+        pawn_left.generate_possible_moves
+        pawn_right.generate_possible_moves
+
+        centre_positions = extract_path_positions(pawn_centre.possible_paths)
+        left_positions = extract_path_positions(pawn_left.possible_paths)
+        right_positions = extract_path_positions(pawn_right.possible_paths)
+        expect(centre_positions).to contain_exactly('b5', 'd5', 'c5')
+        expect(left_positions).to contain_exactly('a5', 'b5')
+        expect(right_positions).to contain_exactly('g4', 'h4')
       end
     end
 
     describe '#en_passant?' do
       context 'for first move' do
-        context 'en_passant flag is set after moving two spaces' do
-          it 'for black pieces' do
-            pawn_centre.move('c5')
-            expect(pawn_centre).to be_en_passant
-          end
-          it 'for white pieces' do
-            pawn_right.move('h4')
-            expect(pawn_right).to be_en_passant
-          end
+        it 'en_passant flag is set after moving two spaces' do
+          pawn_centre.move('c5')
+          pawn_right.move('h4')
+
+          expect(pawn_centre).to be_en_passant
+          expect(pawn_right).to be_en_passant
         end
-        context 'en_passant flag is not set set after moving one space' do
-          it 'for black pieces' do
-            pawn_centre.move('c6')
-            expect(pawn_centre).not_to be_en_passant
-          end
-          it 'for white pieces' do
-            pawn_right.move('h3')
-            expect(pawn_right).not_to be_en_passant
-          end
+        it 'en_passant flag is not set set after moving one space' do
+          pawn_centre.move('c6')
+          pawn_right.move('h3')
+
+          expect(pawn_centre).not_to be_en_passant
+          expect(pawn_right).not_to be_en_passant
         end
       end
       context 'for second move' do
-        context 'en_passant flag is not set after moving three spaces' do
-          it 'for black pieces' do
-            pawn_left.move('a5')
-            pawn_left.move('a4')
-            expect(pawn_left).not_to be_en_passant
-          end
-          it 'for white pieces' do
-            pawn_right.move('h4')
-            pawn_right.move('h5')
-            expect(pawn_right).not_to be_en_passant
-          end
+        it 'en_passant flag is not set after moving three spaces' do
+          pawn_left.move('a5')
+          pawn_left.generate_possible_moves
+          pawn_left.move('a4')
+          pawn_right.move('h4')
+          pawn_right.generate_possible_moves
+          pawn_right.move('h5')
+
+          expect(pawn_left).not_to be_en_passant
+          expect(pawn_right).not_to be_en_passant
         end
-        context 'en_passant flag is not set after moving two spaces' do
-          it 'for black pieces' do
-            pawn_left.move('a6')
-            pawn_left.move('a5')
-            expect(pawn_left).not_to be_en_passant
-          end
-          it 'for white pieces' do
-            pawn_right.move('h3')
-            pawn_right.move('h4')
-            expect(pawn_right).not_to be_en_passant
-          end
+        it 'en_passant flag is not set after moving two spaces' do
+          pawn_left.move('a6')
+          pawn_left.generate_possible_moves
+          pawn_left.move('a5')
+          pawn_right.move('h3')
+          pawn_right.generate_possible_moves
+          pawn_right.move('h4')
+
+          expect(pawn_left).not_to be_en_passant
+          expect(pawn_right).not_to be_en_passant
         end
       end
     end
@@ -128,22 +110,15 @@ describe Pawn do
         pawn_left.possible_paths.append(node)
         pawn_right.possible_paths.append(node)
       end
-      context 'pawn moved to last row' do
-        it 'in center of board' do
-          allow(node).to receive(:find).and_return('a true value')
-          pawn_centre.move('c1')
-          expect(pawn_centre).to be_promoted
-        end
-        it 'on left side of board' do
-          allow(node).to receive(:find).and_return('a true value')
-          pawn_left.move('a1')
-          expect(pawn_left).to be_promoted
-        end
-        it 'on right side of board' do
-          allow(node).to receive(:find).and_return('a true value')
-          pawn_right.move('h8')
-          expect(pawn_right).to be_promoted
-        end
+      it 'pawn moved to last row causes a promotion' do
+        allow(node).to receive(:find).and_return('a true value').exactly(3).times
+        pawn_centre.move('c1')
+        pawn_left.move('a1')
+        pawn_right.move('h8')
+
+        expect(pawn_centre).to be_promoted
+        expect(pawn_left).to be_promoted
+        expect(pawn_right).to be_promoted
       end
       context 'pawn moved to any other row' do
         context 'in center of board' do
