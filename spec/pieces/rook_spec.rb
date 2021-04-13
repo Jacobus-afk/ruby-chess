49a50cbe41ff6ebbeef3c3832cf0e040a55e3395
Jacobus-afk <s24162826@gmail.com>
2021-04-13 23:09:05 +0200

    rook - added test for not first move

--- spec/pieces/rook_spec.rb
+++ spec/pieces/rook_spec.rb
@@ -10,6 +10,7 @@ describe Rook do
   subject(:rook_black_black_square) { described_class.new(BLACK_PIECE, 'h8') }
 
   describe '#generate_possible_moves' do
+    let(:node) { instance_double(Node) }
     it "rook's first move" do
       white_white_square = extract_path_positions(rook_white_white_square.possible_paths)
       white_black_square = extract_path_positions(rook_white_black_square.possible_paths)
@@ -23,6 +24,27 @@ describe Rook do
                                                     'a7', 'a6', 'a5', 'a4', 'a3', 'a2', 'a1')
       expect(black_black_square).to contain_exactly('a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8',
                                                     'h7', 'h6', 'h5', 'h4', 'h3', 'h2', 'h1')
+    end
+    it "not rook's first move" do
+      rook_white_black_square.possible_paths.append(node)
+      rook_black_white_square.possible_paths.append(node)
+      allow(node).to receive(:find).and_return('a valid value').twice
+      rook_white_white_square.move('e1')
+      rook_white_black_square.move('c4')
+      rook_black_white_square.move('e5')
+      rook_black_black_square.move('h5')
+      white_white_square = extract_path_positions(rook_white_white_square.possible_paths)
+      white_black_square = extract_path_positions(rook_white_black_square.possible_paths)
+      black_white_square = extract_path_positions(rook_black_white_square.possible_paths)
+      black_black_square = extract_path_positions(rook_black_black_square.possible_paths)
+      expect(white_white_square).to contain_exactly('a1', 'b1', 'c1', 'd1', 'f1', 'g1', 'h1',
+                                                    'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8')
+      expect(white_black_square).to contain_exactly('a4', 'b4', 'd4', 'e4', 'f4', 'g4', 'h4',
+                                                    'c1', 'c2', 'c3', 'c5', 'c6', 'c7', 'c8')
+      expect(black_white_square).to contain_exactly('a5', 'b5', 'c5', 'd5', 'f5', 'g5', 'h5',
+                                                    'e1', 'e2', 'e3', 'e4', 'e6', 'e7', 'e8')
+      expect(black_black_square).to contain_exactly('a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5',
+                                                    'h1', 'h2', 'h3', 'h4', 'h6', 'h7', 'h8')
     end
   end
 end
