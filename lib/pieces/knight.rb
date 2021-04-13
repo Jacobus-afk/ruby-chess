23dd957a7cb0fa81cb4cf56badad1fddc5af7a06
Jacobus-afk <s24162826@gmail.com>
2021-04-13 22:56:05 +0200

    added implementation and tests for knight generate moves

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -2,9 +2,25 @@
 
 require './lib/pieces/chess_piece'
 
+KNIGHT_VECTORS = [[1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2]].freeze
+
 # Knight class
 class Knight < ChessPiece
   def initialize(team, pos)
     super(team, '♘', pos)
+  end
+
+  def generate_possible_moves
+    super
+    _fill_paths_arr
+  end
+
+  private
+
+  def _fill_paths_arr(tags = %i[check_move check_attack])
+    KNIGHT_VECTORS.each do |vector|
+      path = _create_single_path(vector, tags)
+      @possible_paths.append(path) unless path.nil?
+    end
   end
 end
--- spec/pieces/knight_spec.rb
+++ spec/pieces/knight_spec.rb
@@ -1,1 +1,41 @@
 # frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
+require './lib/pieces/knight'
+require './spec/pieces/linked_list_helper'
+
+describe Knight do
+  subject(:knight_white_white_square) { described_class.new(WHITE_PIECE, 'b1') }
+  subject(:knight_white_black_square) { described_class.new(WHITE_PIECE, 'g1') }
+  subject(:knight_black_white_square) { described_class.new(BLACK_PIECE, 'g8') }
+  subject(:knight_black_black_square) { described_class.new(BLACK_PIECE, 'b8') }
+
+  describe '#generate_possible_moves' do
+    it "knight's first move" do
+      white_white_square = extract_path_positions(knight_white_white_square.possible_paths)
+      white_black_square = extract_path_positions(knight_white_black_square.possible_paths)
+      black_white_square = extract_path_positions(knight_black_white_square.possible_paths)
+      black_black_square = extract_path_positions(knight_black_black_square.possible_paths)
+      expect(white_white_square).to contain_exactly('a3', 'c3', 'd2')
+      expect(white_black_square).to contain_exactly('h3', 'f3', 'e2')
+      expect(black_white_square).to contain_exactly('h6', 'f6', 'e7')
+      expect(black_black_square).to contain_exactly('a6', 'c6', 'd7')
+    end
+    it "not knight's first move" do
+      knight_white_white_square.move('c3')
+      knight_white_black_square.move('h3')
+      knight_black_white_square.move('e7')
+      knight_black_black_square.move('a6')
+      white_white_square = extract_path_positions(knight_white_white_square.possible_paths)
+      white_black_square = extract_path_positions(knight_white_black_square.possible_paths)
+      black_white_square = extract_path_positions(knight_black_white_square.possible_paths)
+      black_black_square = extract_path_positions(knight_black_black_square.possible_paths)
+      expect(white_white_square).to contain_exactly('b1', 'a2', 'a4', 'b5', 'd5', 'e4', 'e2', 'd1')
+      expect(white_black_square).to contain_exactly('g1', 'f2', 'f4', 'g5')
+      expect(black_white_square).to contain_exactly('g8', 'g6', 'f5', 'd5', 'c6', 'c8')
+      expect(black_black_square).to contain_exactly('b8', 'c7', 'c5', 'b4')
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
