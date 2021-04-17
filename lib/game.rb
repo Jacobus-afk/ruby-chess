fc4fc1c9beee7ad31ff0023ac400bd5ce5bbff37
Jacobus-afk <s24162826@gmail.com>
2021-04-17 17:33:31 +0200

    added different piece states. still need to write tests (incomplete)

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,98 @@
 # frozen_string_literal: true
 
-require './lib/keys.rb'
+require './lib/keys'
+require './lib/board'
 
+SELECT_PIECE = 0
+MOVE_PIECE = 1
+
+PIECE_METHOD = { SELECT_PIECE => :_select_piece,
+                 MOVE_PIECE => :_move_piece }.freeze
 # game class
 class Game
   include Keypress
 
-  def player_input
+  def initialize
+    @board = Board.new
+    @current_player = _find_active_player
+    @piecestate = SELECT_PIECE
+    # @piecemethodhash = { SELECT_PIECE => _select_piece,
+    #                      MOVE_PIECE => _move_piece }
+    @valid_move = false
+    @current_piece = nil
+    @active_game = true
+  end
+
+  def handle_player_input
+    _handle_command(_player_input)
+  end
+
+  def start_game
+    @board.start_game
+    loop do
+      _draw_board
+      _handle_command(_player_input)
+      break unless @active_game
+    end
+  end
+
+  private
+
+  def _draw_board
+    system('clear') || system('cls')
+    puts @board.draw_board
+  end
+
+  def _toggle_piecestate
+    @piecestate = (@piecestate + 1) % 2
+  end
+
+  def _select_piece
+    pos = @current_player.position
+    piece = @board.pieces.fetch(pos, nil)
+    return false if piece.nil?
+
+    @board.tile_selection = @current_player.coordinate
+  end
+
+  def _move_piece
+    true
+  end
+
+  def _find_active_player
+    @board.players.each do |player|
+      return player if player.active
+    end
+  end
+
+  def _player_input
     loop do
       input = read_single_key
       return input unless input.nil?
     end
   end
+
+  def _handle_enter
+    _toggle_piecestate if send(PIECE_METHOD[@piecestate])
+  end
+
+  def _handle_esc; end
+
+  def _handle_move(move)
+    @current_player.send(move)
+  end
+
+  def _handle_command(input)
+    return if input.nil?
+
+    _handle_enter if input == :enter
+    _handle_esc if input == :esc
+
+    _handle_move(input) if %i[move_left move_right move_up move_down].include?(input)
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   game = Game.new
-  puts game.player_input
+  puts game.start_game
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,7 +5,16 @@ require './lib/game'
 describe Game do
   subject(:game) { described_class.new }
 
-  describe '#player_input' do
+  context 'when instantiating class' do
+    it 'valid_move is false' do
+      expect(game.instance_variable_get(:@valid_move)).to be false
+    end
+    it 'adds a board class instance' do
+      expect(game.instance_variable_get(:@board)).to be_a(Board)
+    end
+  end
+
+  describe '#handle_player_input' do
     before(:each) do
       allow(game).to receive(:loop).and_yield.and_yield.and_yield
     end
@@ -13,13 +22,13 @@ describe Game do
     it 'breaks the loop when valid input is made' do
       allow(game).to receive(:read_single_key).and_return(:escape)
       expect(game).to receive(:read_single_key).once
-      game.player_input
+      game.handle_player_input
     end
 
     it 'loops until a valid input is given' do
       allow(game).to receive(:read_single_key).and_return(nil, nil, nil)
       expect(game).to receive(:read_single_key).exactly(3).times
-      game.player_input
+      game.handle_player_input
     end
   end
 end
