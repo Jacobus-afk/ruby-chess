29e9aec7ce86b8aaaf87fef0b0f5f92728670561
Jacobus-afk <s24162826@gmail.com>
2021-04-17 13:16:36 +0200

    refactored board_spec to make it more readable

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,6 +16,16 @@ describe Board do
   white = "\e[30;107m   \e[0m"
   black = "\e[30;100m   \e[0m"
   red = "\e[30;101m   \e[0m"
+
+  board_template = ["#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
+                    "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
+                    "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
+                    "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
+                    "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
+                    "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
+                    "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
+                    "#{red}#{white}#{black}#{white}#{black}#{white}#{black}#{white}"]
+
   context 'when instantiating class' do
     it 'initializes variables correcty' do
       expect(board.pieces).to eql({})
@@ -31,59 +41,56 @@ describe Board do
   describe '#draw_board' do
     let(:chesspiece) { instance_double(ChessPiece, coordinate: [1, 2], unicode: '♘') }
     it 'draws template correctly' do
-      expect(board.draw_board).to eql(["#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
-                                       "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
-                                       "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
-                                       "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{red}#{white}#{black}#{white}#{black}#{white}#{black}#{white}"])
+      expect(board.draw_board).to eql(board_template)
     end
 
     it 'handles inactive pieces correctly' do
       board.instance_variable_set(:@pieces, { 'a8' => chesspiece })
       allow(chesspiece).to receive(:active?).and_return(false)
-
-      expect(board.draw_board).to eql(["#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
-                                       "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
-                                       "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
-                                       "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{red}#{white}#{black}#{white}#{black}#{white}#{black}#{white}"])
+      expect(board.draw_board).to eql(board_template)
     end
 
     it 'handles active pieces correctly' do
       board.instance_variable_set(:@pieces, { 'c7' => chesspiece })
       allow(chesspiece).to receive(:active?).and_return(true)
-
-      expect(board.draw_board).to eql(["#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{black}#{white}" + ' ♘ '.black_bg + "#{white}#{black}#{white}#{black}#{white}",
-                                       "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
-                                       "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
-                                       "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                       "#{red}#{white}#{black}#{white}#{black}#{white}#{black}#{white}"])
+      # board_active = board_template.dup
+      board_template[1] = "#{black}#{white}" + ' ♘ '.black_bg + "#{white}#{black}#{white}#{black}#{white}"
+      expect(board.draw_board).to eql(board_template)
+      # expect(board.draw_board).to eql(["#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
+      #                                  "#{black}#{white}" + ' ♘ '.black_bg + "#{white}#{black}#{white}#{black}#{white}",
+      #                                  "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
+      #                                  "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
+      #                                  "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
+      #                                  "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
+      #                                  "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
+      #                                  "#{red}#{white}#{black}#{white}#{black}#{white}#{black}#{white}"])
     end
   end
 
   describe '#start_game' do
     it 'draws pieces for a new game correctly' do
-      expect(board.start_game).to eq([' ♜ '.white_bg + ' ♞ '.black_bg + ' ♝ '.white_bg + ' ♛ '.black_bg +
-                                       ' ♚ '.white_bg + ' ♝ '.black_bg + ' ♞ '.white_bg + ' ♜ '.black_bg,
-                                      ' ♟ '.black_bg + ' ♟ '.white_bg + ' ♟ '.black_bg + ' ♟ '.white_bg +
-                                      ' ♟ '.black_bg + ' ♟ '.white_bg + ' ♟ '.black_bg + ' ♟ '.white_bg,
-                                      "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                      "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
-                                      "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
-                                      "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
-                                      ' ♙ '.white_bg + ' ♙ '.black_bg + ' ♙ '.white_bg + ' ♙ '.black_bg +
-                                      ' ♙ '.white_bg + ' ♙ '.black_bg + ' ♙ '.white_bg + ' ♙ '.black_bg,
-                                      ' ♖ '.red_bg + ' ♘ '.white_bg + ' ♗ '.black_bg + ' ♕ '.white_bg +
-                                      ' ♔ '.black_bg + ' ♗ '.white_bg + ' ♘ '.black_bg + ' ♖ '.white_bg])
+      # board_sta
+      board_template[0] = ' ♜ '.white_bg + ' ♞ '.black_bg + ' ♝ '.white_bg + ' ♛ '.black_bg +
+                          ' ♚ '.white_bg + ' ♝ '.black_bg + ' ♞ '.white_bg + ' ♜ '.black_bg
+      board_template[1] = ' ♟ '.black_bg + ' ♟ '.white_bg + ' ♟ '.black_bg + ' ♟ '.white_bg +
+                          ' ♟ '.black_bg + ' ♟ '.white_bg + ' ♟ '.black_bg + ' ♟ '.white_bg
+      board_template[6] = ' ♙ '.white_bg + ' ♙ '.black_bg + ' ♙ '.white_bg + ' ♙ '.black_bg +
+                          ' ♙ '.white_bg + ' ♙ '.black_bg + ' ♙ '.white_bg + ' ♙ '.black_bg
+      board_template[7] = ' ♖ '.red_bg + ' ♘ '.white_bg + ' ♗ '.black_bg + ' ♕ '.white_bg +
+                          ' ♔ '.black_bg + ' ♗ '.white_bg + ' ♘ '.black_bg + ' ♖ '.white_bg
+      expect(board.start_game).to eql(board_template)
+      # expect(board.start_game).to eq([' ♜ '.white_bg + ' ♞ '.black_bg + ' ♝ '.white_bg + ' ♛ '.black_bg +
+      #                                  ' ♚ '.white_bg + ' ♝ '.black_bg + ' ♞ '.white_bg + ' ♜ '.black_bg,
+      #                                 ' ♟ '.black_bg + ' ♟ '.white_bg + ' ♟ '.black_bg + ' ♟ '.white_bg +
+      #                                 ' ♟ '.black_bg + ' ♟ '.white_bg + ' ♟ '.black_bg + ' ♟ '.white_bg,
+      #                                 "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
+      #                                 "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
+      #                                 "#{white}#{black}#{white}#{black}#{white}#{black}#{white}#{black}",
+      #                                 "#{black}#{white}#{black}#{white}#{black}#{white}#{black}#{white}",
+      #                                 ' ♙ '.white_bg + ' ♙ '.black_bg + ' ♙ '.white_bg + ' ♙ '.black_bg +
+      #                                 ' ♙ '.white_bg + ' ♙ '.black_bg + ' ♙ '.white_bg + ' ♙ '.black_bg,
+      #                                 ' ♖ '.red_bg + ' ♘ '.white_bg + ' ♗ '.black_bg + ' ♕ '.white_bg +
+      #                                 ' ♔ '.black_bg + ' ♗ '.white_bg + ' ♘ '.black_bg + ' ♖ '.white_bg])
     end
   end
 end
